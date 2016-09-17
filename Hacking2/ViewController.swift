0e21f06cfa6e1655b8fe51d1d7e404fb07a6522d
Macpeters <peters.maryanne@gmail.com>
2016-09-17 17:54:52 -0400

    show a random flag each time

--- Hacking2/ViewController.swift
+++ Hacking2/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var button1: UIButton!
+  @IBOutlet weak var button2: UIButton!
+  @IBOutlet weak var button3: UIButton!
+  
+  var countries = [String]()
+  var score = 0
+  var correctAnswer = 0
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    countries.append("estonia")
+    countries.append("france")
+    countries.append("germany")
+    countries.append("ireland")
+    countries.append("italy")
+    countries.append("monaco")
+    countries.append("nigeria")
+    countries.append("poland")
+    countries.append("russia")
+    countries.append("spain")
+    countries.append("uk")
+    countries.append("us")
+    
+    button1.layer.borderWidth = 1
+    button2.layer.borderWidth = 1
+    button3.layer.borderWidth = 1
+    
+    button1.layer.borderColor = UIColor.lightGray.cgColor
+    button2.layer.borderColor = UIColor.lightGray.cgColor
+    button3.layer.borderColor = UIColor.lightGray.cgColor
+    
+    askQuestion()
+  }
+  
+  func askQuestion(){
+    // shuffle the countries (part of GameplayKit)
+    countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+    
+    button1.setImage(UIImage(named: countries[0]), for: .normal)
+    button2.setImage(UIImage(named: countries[1]), for: .normal)
+    button3.setImage(UIImage(named: countries[2]), for: .normal)
   }
 
   override func didReceiveMemoryWarning() {
